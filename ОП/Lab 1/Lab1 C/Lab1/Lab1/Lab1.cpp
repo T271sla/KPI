5e99bf3e1f278c33f3f03deb2d8fb88c1a62fba3
Kirill Tesla <89296895+T271sla@users.noreply.github.com>
2021-09-14 10:33:58 +0300

    Update Lab1.cpp

--- ОП/Lab 1/Lab1 C/Lab1/Lab1/Lab1.cpp
+++ ОП/Lab 1/Lab1 C/Lab1/Lab1/Lab1.cpp
@@ -9,15 +9,18 @@ int main()
     double V1, V2;
     const float PI = 3.14;
 
+    #Введення даних
     printf("Enter radius(R):");
     scanf_s("%d", &R);
     printf("Enter height(H):");
     scanf_s("%d", &H);
 
+    #Розрахунок
     V1 = R * R * H * PI;
     V2 = V1 / 3;
 
+    #Виведення даних
     printf("Volume of the cylinder:%d\n", V1);
-    printf("Volume of the cone:%d\n", V2);//вывод данных
+    printf("Volume of the cone:%d\n", V2);
     system("pause");
 }

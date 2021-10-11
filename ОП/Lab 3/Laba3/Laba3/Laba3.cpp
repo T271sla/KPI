3bb565b3e0290adfb7d8875d7ecc0d9420fa58ff
Kirill Tesla <89296895+T271sla@users.noreply.github.com>
2021-10-11 22:34:00 +0300

    Add files via upload

---
+++ ОП/Lab 3/Laba3/Laba3/Laba3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+#define PI 3.14159265
+
+int main()
+{
+	double x;
+	double fact = 1;                   
+	double sum;
+	double sum_2 = 1;
+	int n = 1;     
+	double eps = 0.000001;
+
+	cout << "Enter x:";
+	cin >> x;
+	do {
+			fact *= (2 * n - 1) * 2 * n;                           
+			sum = (pow(-1, n) * pow(x, (2 * n)) / fact);          
+			if (sum < 1 | sum > -1)                                       
+			{
+				sum_2 += sum;
+			}
+			n += 1;
+		} while (abs(sum) > eps);                       
+
+	cout << "Cos(x) = " << sum_2 << endl;
+	system("pause");
+	return 0;
+}

470daec86d8bb8d80c27a0685cf1c3ffa69b1e7c
Kirill Tesla <89296895+T271sla@users.noreply.github.com>
2021-11-02 11:12:40 +0300

    Add files via upload

---
+++ ОП/Lab 4/Lab 4/Lab 4/Lab 4/Lab 4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double sum_1 = 0;
+	double sum_2 = 0;
+	float n_1, n_2;
+
+	for (n_1 = 1; n_1 <= 100; n_1 += 1)
+		sum_1 += (pow(-1, n_1 + 1) * (1 / n_1));
+	cout << "First sum = " << sum_1 << endl;
+
+	for (n_2 = 100; n_2 >= 1; n_2 -= 1)
+		sum_2 += (pow(-1, n_2 + 1) * (1 / n_2));
+	cout << "Second sum = " << sum_2 << endl;
+
+	if (sum_1 > sum_2) {
+		cout << "First result is greater" << endl;
+	}
+	else if (sum_1 < sum_2) {
+		cout << "Second result is greater" << endl;
+	}
+	else cout << "Results are the same" << endl;
+
+	system("pause");
+	return 0;
+}

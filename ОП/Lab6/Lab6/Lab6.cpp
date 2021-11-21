13348749c881cd22297932db68730ee55b7eb7ba
Kirill Tesla <89296895+T271sla@users.noreply.github.com>
2021-11-21 17:12:29 +0300

    Add files via upload

---
+++ ОП/Lab6/Lab6/Lab6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int func(int n)
+{
+	do {
+		int res;
+		n /= 10;
+	} while (n > 10);
+
+	return n;
+}
+
+int main()
+{
+	int a, b, c, sum;
+	cout << "Enter a, b, c" << endl;
+	cin >> a >> b >> c;
+	sum = func(a) + func(b) + func(c);
+	cout << "Sum of first digits of entered numbers is:" << endl;
+	cout << sum << endl;
+	
+	system("pause");
+	return 0;
+}

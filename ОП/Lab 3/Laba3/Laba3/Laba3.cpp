c754fdbac7d506beb5289f55ab37b6ac9f8c1ce4
Kirill Tesla <89296895+T271sla@users.noreply.github.com>
2021-11-21 18:09:06 +0200

    Update Laba3.cpp

--- ОП/Lab 3/Laba3/Laba3/Laba3.cpp
+++ ОП/Lab 3/Laba3/Laba3/Laba3.cpp
@@ -1,31 +1,29 @@
-﻿#include <iostream>
-#include <cmath>
+#include <iostream>
 using namespace std;
 
-#define PI 3.14159265
+int func(int n)
+{
+	do {
+		n /= 10;
+	} while (n > 10);
+
+	return n;
+}
 
 int main()
 {
-	double x;
-	double fact = 1;                   
-	double sum;
-	double sum_2 = 1;
-	int n = 1;     
-	double eps = 0.000001;
-
-	cout << "Enter x:";
-	cin >> x;
-	do {
-			fact *= (2 * n - 1) * 2 * n;                           
-			sum = (pow(-1, n) * pow(x, (2 * n)) / fact);          
-			if (sum < 1 | sum > -1)                                       
-			{
-				sum_2 += sum;
-			}
-			n += 1;
-		} while (abs(sum) > eps);                       
-
-	cout << "Cos(x) = " << sum_2 << endl;
+	int a, b, c, sum;
+	cout << "Enter a, b, c ∈ [1; +inf)" << endl;
+	cin >> a >> b >> c;
+	if (a >= 1 && b >= 1 && c >= 1) {
+		sum = func(a) + func(b) + func(c);
+		cout << "Sum of first digits of entered numbers is:" << endl;
+		cout << sum << endl;
+	}
+	else {
+		cout << "One number (or more) doesn't belong to the required range. Try again." << endl;
+	}
+	
 	system("pause");
 	return 0;
 }

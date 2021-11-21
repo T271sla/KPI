f5c92aeeb0739117865ffc9847415ba2ebe7208f
Kirill Tesla <89296895+T271sla@users.noreply.github.com>
2021-11-21 19:50:11 +0200

    Update Lab6.cpp

--- ОП/Lab6/Lab6/Lab6.cpp
+++ ОП/Lab6/Lab6/Lab6.cpp
@@ -1,24 +1,29 @@
-﻿#include <iostream>
-#include <math.h>
+#include <iostream>
 using namespace std;
 
 int func(int n)
 {
-	do {
-		n /= 10;
-	} while (n > 10);
-
-	return n;
+   int k;
+   do {
+      k=n;
+      n/=10;
+   }while(n);
+   return k;
 }
 
 int main()
 {
 	int a, b, c, sum;
-	cout << "Enter a, b, c" << endl;
+	cout << "Enter a, b, c ∈ [1; +inf)" << endl;
 	cin >> a >> b >> c;
-	sum = func(a) + func(b) + func(c);
-	cout << "Sum of first digits of entered numbers is:" << endl;
-	cout << sum << endl;
+	if (a > 0 && b > 0 && c > 0) {
+		sum = func(a) + func(b) + func(c);
+		cout << "Sum of first digits of entered numbers is:" << endl;
+		cout << sum << endl;
+	}
+	else {
+		cout << "One number (or more) doesn't belong to the required range. Try again." << endl;
+	}
 	
 	system("pause");
 	return 0;

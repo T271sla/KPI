14d069d54d1d5536dbe586cce37b4f1003a39c0e
Kirill Tesla <89296895+T271sla@users.noreply.github.com>
2022-04-12 10:28:49 +0300

    Update Header.h

--- ОП 12/Lab 1/Lab2'1/Header.h
+++ ОП 12/Lab 1/Lab2'1/Header.h
@@ -8,7 +8,7 @@ using namespace std;
 
 string fill_og();
 int fill_files(string path_1, string path_2, string path_3, int rows);
-string create_array(string S, int size);
+string create_array(string S, int& sz);
 void change(string* S, int size);
 void replace_uneven(string path_4, string path_3);
 string create_array_2(int size_2, string even_str, int& wrd);

9640f72ff3a9e2d767e8ac78df0abd70f744beec
Kirill Tesla <89296895+T271sla@users.noreply.github.com>
2022-06-07 10:49:41 +0300

    Add files via upload

---
+++ ОП 12/Lab 2'6/Lab 2'6/Lab 2'6.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+
+struct node
+{
+    int info;
+    node* l, * r;
+};
+
+node* tree = NULL;
+
+void push(int a, node** t)
+{
+    if ((*t) == NULL)
+    {
+        (*t) = new node;
+        (*t)->info = a;
+        (*t)->l = (*t)->r = NULL;
+        return;
+    }
+
+    if (a > (*t)->info) {
+        push(a, &(*t)->r);
+    }
+    else
+    {
+        push(a, &(*t)->l);
+    }
+}
+
+int inOrder(node* tree, int& max)
+{
+    if (tree != NULL)
+    {
+        if (tree->r == NULL)
+        {
+            max = tree->info;
+        }
+        inOrder(tree->r, max);
+    }
+    return max;
+}
+
+int main()
+{
+    int n;
+    int s;
+    cout << "How many elements: ";
+    cin >> n;
+
+
+    node* tree = NULL;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << "Enter element: ";
+        cin >> s;
+        push(s, &tree);
+    }
+
+    int max = 0;
+    int res = inOrder(tree, max);
+    cout << "Max element: " << res;
+}
